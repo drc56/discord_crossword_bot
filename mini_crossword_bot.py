0cbbcfbe311605ed2ffaf197cdab746418da4853
Dan Cuomo <dcuomo56@DESKTOP-CEV4SBD.localdomain>
2022-05-20 14:29:03 -0400

    some cleanup and removing that screte key whoops

--- mini_crossword_bot.py
+++ mini_crossword_bot.py
@@ -3,18 +3,51 @@ import datetime
 import logging
 import re
 import sqlite3
+from pathlib import Path
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 
-# TODO store these in an environment file and load it in
-TOKEN = 'OTc3MTg5MTU2NTE2MTQ3MjUw.G8uDQB.m8WBM2He1MYoSLb1A4_PBuH65cjxKpMPy3dKDE'
+TOKEN = Path('secret_token.txt').read_text()
 db_con = sqlite3.connect('scores.db')
 bot = commands.Bot(command_prefix='!')
+
+def determine_date() -> str:
+    """The NYT does crossword resets on 10PM EST on weekdays and 6PM EST on weekends.
+    So a score after that time is assumed to be a puzzle for the next day
+    """
+    date = datetime.datetime.now()
+    if date.weekday() > 4:
+        if date.time() >= datetime.time(18,0,0):
+            return str(date.date() + datetime.timedelta(days=1))
+        else:
+            return str(date.date()) 
+    else:
+        if date.time() >= datetime.time(22,0,0):
+            return str(date.date() + datetime.timedelta(days=1))
+        else:
+            return str(date.date()) 
+
+def get_word_games_chan_id() -> id:
+    for chan in bot.get_all_channels():
+        if chan.name == 'word-games':
+            return chan
+
+@tasks.loop(hours=1)
+async def remind_for_scores():
+    date = datetime.datetime.now()
+    if date.weekday() > 4:
+        if date.time() >= datetime.time(17,0,0):
+            chan = get_word_games_chan_id()
+            await chan.send("REMINDER: Complete and submit crossword scores")
+    else:
+        if date.time() >= datetime.time(21,0,0):
+            chan = get_word_games_chan_id()
+            await chan.send("REMINDER: Complete and submit crossword scores")
 
 @bot.command(name='mini-leader', help="Responds with today's leaderboard")
 async def handle_leaderboard(ctx):
     cur = db_con.cursor()
-    date = str(datetime.date.today())
+    date = determine_date()
     leader_cmd = 'SELECT * FROM scores WHERE date = ? ORDER BY score'
     cur.execute(leader_cmd, [date])
     rows = cur.fetchall()
@@ -38,7 +71,7 @@ async def handle_mini_score(ctx):
     else:
         time = int(m.group(1)) * 60 + int(m.group(2))
         user = str(ctx.message.author)
-        date = str(datetime.date.today())
+        date = determine_date()
 
         # Check if the user already has a score
         check_cmd = 'SELECT * from scores WHERE user = ? AND date = ?'
@@ -67,7 +100,7 @@ async def handle_mini_correct(ctx):
     else:
         time = int(m.group(1)) * 60 + int(m.group(2))
         user = str(ctx.message.author)
-        date = str(datetime.date.today())
+        date = determine_date()
 
         # Check if the user already has a score
         check_cmd = 'SELECT * from scores WHERE user = ? AND date = ?'
@@ -89,6 +122,6 @@ def main():
     logging.basicConfig(filename='crossword_bot.log', level=logging.DEBUG)
     logging.info('Starting bot')
     bot.run(TOKEN)
-
+    
 if __name__ == '__main__':
-    main()
+    main() 
