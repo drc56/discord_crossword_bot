9342fb6aa13c810401d638f9afe1ab91c7eb5ae2
Dan Cuomo <dcuomo56@gmail.com>
2022-06-17 08:44:17 -0400

    I never remembered to start the updater, actually starting it now

--- mini_crossword_bot.py
+++ mini_crossword_bot.py
@@ -226,6 +226,7 @@ async def handle_mini_stats(ctx):
 def main():
     logging.basicConfig(filename='crossword_bot.log', level=logging.INFO)
     logging.info('Starting the crossword bot')
+    daily_updater = DailyUpdater()
     bot.run(TOKEN)
     
 if __name__ == '__main__':
