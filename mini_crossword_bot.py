a52a4c44a603d7eabe423e6fc0b39544ba0c17bf
Dan Cuomo <dcuomo56@gmail.com>
2022-06-18 09:30:18 -0400

    adding a reminder to the crossword bot

--- mini_crossword_bot.py
+++ mini_crossword_bot.py
@@ -1,9 +1,11 @@
 # /bin/python3
 
+import asyncio
 import datetime
 import logging
 import re
 import sqlite3
+from venv import create
 import pytz
 import time
 from dataclasses import dataclass
@@ -19,6 +21,7 @@ from discord.ext import commands, tasks
 TOKEN = Path('secret_token.txt').read_text()
 db_con = sqlite3.connect('scores.db')
 bot = commands.Bot(command_prefix='!')
+ROLE_NAME = "crossword_players"
 
 # Helper functions and data types
 @dataclass
@@ -47,11 +50,30 @@ def determine_date(today: bool = True) -> str:
         else:
             return str(date.date()) if today else (str(date.date() - datetime.timedelta(days=1)))
 
+def should_remind(today: bool = True) -> str:
+    """The NYT does crossword resets on 10PM EST on weekdays and 6PM EST on weekends.
+    So a score after that time is assumed to be a puzzle for the next day
+    """
+    et = pytz.timezone('US/Eastern')
+    date = datetime.datetime.now().astimezone(et)
+
+    if date.weekday() > 4:
+        if date.time() >= datetime.time(17,0,0):
+            return True
+        else:
+            return False
+    else:
+        if date.time() >= datetime.time(21,0,0):
+            return True
+        else:
+            return False
+
 class DailyUpdater(commands.Cog):
     def __init__(self):
         logging.info("Spinning up the updater")
         self.date = determine_date()
         self.update_winner_table.start()
+        self.remind_users.start()
     
     @tasks.loop(hours=1.0)
     async def update_winner_table(self):
@@ -82,10 +104,39 @@ class DailyUpdater(commands.Cog):
                 cur.execute("INSERT INTO winners values (?, ?)", [str(winner), str(self.date)])
             self.date = cur_date
         return
+    
+    @staticmethod
+    def get_chan_id() -> id:
+        for chan in bot.get_all_channels():
+            if chan.name == "word-games":
+                return chan
+
+    @tasks.loop(hours=1.0)
+    async def remind_users(self):
+        if True:
+            chan = self.get_chan_id()
+            # TODO make this not one guild specific...
+            guild = None
+            for guild_id in bot.guilds:
+                if str(guild_id) == "Queue Tip Bandits":
+                    guild = guild_id
+                    break
+            role_id = None
+            for role in guild.roles:
+                if str(role) == ROLE_NAME:
+                    role_id = role
+                    break
+            if role_id:
+                await chan.send(f"Don't forget to submit your crossword scores <@&{role_id.id}>")
 
     @update_winner_table.before_loop
     async def before_start(self):
         await bot.wait_until_ready()
+
+    @remind_users.before_loop
+    async def before_start(self):
+        await bot.wait_until_ready()
+
 
 def get_word_games_chan_id() -> id:
     for chan in bot.get_all_channels():
@@ -156,9 +207,23 @@ def delete_score(score : LeaderboardEntry):
     cur.execute(delete_cmd,[score.user,score.date])
     db_con.commit()
 
+async def create_crossword_role(guild : discord.guild):
+    logging.info("Attempting to create a guild")
+    print(guild)
+    for role in guild.roles:
+        if str(role) == ROLE_NAME:
+            return
+    await guild.create_role(name=ROLE_NAME)
+
+async def create_crossword_role_all_guilds():
+    logging.info("create all the roles")
+    tasks = [create_crossword_role(guild) for guild in bot.guilds]
+    await asyncio.wait(tasks)
+
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name="to !help"))
+    tasks = [create_crossword_role_all_guilds(), bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name="to !help"))]
+    await asyncio.wait(tasks)
 
 @bot.command(name='mini-leader', help="Responds with today's leaderboard")
 async def handle_leaderboard(ctx):
@@ -222,9 +287,34 @@ async def handle_mini_stats(ctx):
 
     await ctx.send(stats_string)
 
+@bot.command(name='mini-join-reminder', help='Join the mini crossword reminder group')
+async def handle_mini_join(ctx):
+    guild = bot.get_guild(ctx.guild.id)
+    role = None
+    for role_it in guild.roles:
+        if str(role_it) == ROLE_NAME:
+            role = role_it
+    if not role:
+        logging.error("The crossword role does not exist can't add user")
+        await ctx.send("Error joining crossword reminder group, might not exist, contact admin")
+    tasks = [ctx.message.author.add_roles(role), ctx.send(f"Adding {ctx.message.author} to the mini crossword reminder group")]
+    await asyncio.wait(tasks)
+
+@bot.command(name='mini-leave-reminder', help='Leave the mini crossword reminder group')
+async def handle_mini_join(ctx):
+    guild = bot.get_guild(ctx.guild.id)
+    role = None
+    for role_it in guild.roles:
+        if str(role_it) == ROLE_NAME:
+            role = role_it
+    if not role:
+        logging.error("The crossword role does not exist can't add user")
+        await ctx.send("Error deleting crossword reminder group, might not exist, contact admin")
+    tasks = [ctx.message.author.remove_roles(role), ctx.send(f"Removing {ctx.message.author} from the mini crossword reminder group")]
+    await asyncio.wait(tasks)
 
 def main():
-    logging.basicConfig(filename='crossword_bot.log', level=logging.INFO)
+    # logging.basicConfig(filename='crossword_bot.log', level=logging.INFO)
     logging.info('Starting the crossword bot')
     daily_updater = DailyUpdater()
     bot.run(TOKEN)
