e8b75fd266805e7263481d7f31809bee44cebb44
Dan Cuomo <dcuomo56@gmail.com>
2022-07-30 16:03:02 -0400

    added a bunch more testing and fixtures

--- test/fixtures.py
+++ test/fixtures.py
@@ -21,11 +21,30 @@ def discord_bot_mocker():
     bot = MockBot()
     yield bot
 
+# Mocks are just gonna return true or results, this is not for checking db logic
 class LeaderboardDatabaseConnectionMock(LeaderboardDatabaseConnection):
     def __init__(self, database_loc: str, mock_results):
         self._mock_results = mock_results
         pass
     
     def get_scores_for_date(self, date: str):
-        print(self._mock_results)
         return self._mock_results
+    
+    def get_user_stats(self, user: str):
+        return self._mock_results
+        
+    def check_for_existing_score(self, score):
+        return False
+    
+    def insert_score(self, score):
+        return True
+
+    def delete_score(self, score):
+        return False
+
+class LeaderboardDatabaseConnectionExistsMock(LeaderboardDatabaseConnectionMock):
+    def __init__(self, database_loc: str, mock_results):
+        super().__init__(database_loc, mock_results)
+    
+    def check_for_existing_score(self, score):
+        return True
